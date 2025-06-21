4d34cdee5dee1c75c5d674d49f182d16b2b3a43b
biosero-simon <simoncoelho@biosero.com>
2025-06-21 16:59:28 -0700

    feat: add in basic mcp server

---
+++ DOTNET/robot-arm-mcp/Helpers/JointPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robot_arm_mcp.Helpers
+{
+    public class JointPosition
+    {
+        string Joint1 { get; set; }
+        string Joint2 { get; set; }
+        string Joint3 { get; set; }
+        string Joint4 { get; set; }
+        string Joint5 { get; set; }
+        string Joint6 { get; set; }
+        public JointPosition(string joint1, string joint2, string joint3, string joint4, string joint5, string joint6)
+        {
+            Joint1 = joint1;
+            Joint2 = joint2;
+            Joint3 = joint3;
+            Joint4 = joint4;
+            Joint5 = joint5;
+            Joint6 = joint6;
+        }
+
+    }
+}
---
+++ DOTNET/robot-arm-mcp/Tools/PickAndPlaceTool.cs
@@ -0,0 +1,17 @@
+﻿using ModelContextProtocol.Server;
+using robot_arm_mcp.Helpers;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+
+[McpServerToolType]
+public static class PickAndPlaceTool
+{
+    [McpServerTool, Description("Picks from Location A, Closes the gripper, Places at Location B, Opens the Gripper")]
+    public static string PickAndPlace(JointPosition jointALocations, JointPosition jointBLocations)
+    {
+
+
+
+        return "Robot moved plate.";
+    }
+}
