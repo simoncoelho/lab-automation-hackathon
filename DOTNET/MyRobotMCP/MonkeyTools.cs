7277d702dcc7d1ffbd01ade71a23673518f7ba68
biosero-simon <simoncoelho@biosero.com>
2025-06-21 19:59:06 -0700

    feat: add in sample monkey service

---
+++ DOTNET/MyRobotMCP/MonkeyTools.cs
@@ -0,0 +1,25 @@
+﻿using ModelContextProtocol.Server;
+using MyRobotMCP;
+using System;
+using System.ComponentModel;
+using System.Text.Json;
+
+namespace MyRobotMCP;
+
+[McpServerToolType]
+public static class MonkeyTools
+{
+    [McpServerTool, Description("Get a list of monkeys.")]
+    public static string GetMonkeys(MonkeyService monkeyService)
+    {
+        var monkeys = monkeyService.GetMonkeys().GetAwaiter().GetResult();
+        return JsonSerializer.Serialize(monkeys);
+    }
+
+    [McpServerTool, Description("Get a monkey by name.")]
+    public static string GetMonkey(MonkeyService monkeyService, [Description("The name of the monkey to get details for")] string name)
+    {
+        var monkey = monkeyService.GetMonkey(name).GetAwaiter().GetResult();
+        return JsonSerializer.Serialize(monkey);
+    }
+}
---
+++ DOTNET/MyRobotMCP/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using MyRobotMCP;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<MonkeyService>();
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
