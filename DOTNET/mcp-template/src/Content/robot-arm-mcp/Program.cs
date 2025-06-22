6700c2c79b25de49f7ad25848963f53aa6c2e7e2
biosero-simon <simoncoelho@biosero.com>
2025-06-22 01:28:35 -0700

    feat: add in template stuff

---
+++ DOTNET/mcp-template/src/Content/robot-arm-mcp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+// Create a generic host builder for
+// dependency injection, logging, and configuration.
+var builder = Host.CreateApplicationBuilder(args);
+
+// Configure logging for better integration with MCP clients.
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+// Register the MCP server and configure it to use stdio transport.
+// Scan the assembly for tool definitions.
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddSingleton<RobotController>();
+
+// Build and run the host. This starts the MCP server.
+await builder.Build().RunAsync();
